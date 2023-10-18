8cf5d47b974a9380964f3a518f0ac36b3e9cad29
WilliamJSS <williamsena088@gmail.com>
2023-10-18 15:16:51 -0300

    fix: read config files

--- src/main/java/williamjss/model/Config.java
+++ src/main/java/williamjss/model/Config.java
@@ -3,49 +3,47 @@ package williamjss.model;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 
 public class Config {
 
-    private static File defaultScenesFile = new File("src/resources/config/scenes.default.json");
+    private static File scenesFile;
+    private static File defaultScenesFile;
+    private static JsonArray scenes;
     private static JsonArray defaultScenes;
 
-    private static File scenesFile = new File("src/resources/config/scenes.json");
-    private static JsonArray scenes;
+    private static File getFile(String name) {
+        URL resource = Config.class.getClassLoader().getResource("config/" + name);
+        try {
+            return new File(resource.toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public static File getScenesFile() {
+        if (scenesFile == null) {
+            scenesFile = getFile("scenes.json");
+        }
         return scenesFile;
     }
 
     public static File getDefaultScenesFile() {
-        return defaultScenesFile;
-    }
-
-    public static JsonArray getDefaultScenes() {
-        if (defaultScenes == null) {
-            try {
-                FileReader fr = new FileReader(defaultScenesFile);
-                BufferedReader br = new BufferedReader(fr);
-
-                defaultScenes = JsonParser.parseReader(br).getAsJsonArray();
-
-                br.close();
-                fr.close();
-
-            } catch (Exception e) {
-                return null;
-            }
+        if (defaultScenesFile == null) {
+            defaultScenesFile = getFile("scenes.default.json");
         }
-
-        return defaultScenes;
+        return defaultScenesFile;
     }
 
     public static JsonArray getScenes() {
         if (scenes == null) {
             try {
-                FileReader fr = new FileReader(scenesFile);
+                FileReader fr = new FileReader(getScenesFile());
                 BufferedReader br = new BufferedReader(fr);
 
                 scenes = JsonParser.parseReader(br).getAsJsonArray();
@@ -61,4 +59,22 @@ public class Config {
         return scenes;
     }
 
+    public static JsonArray getDefaultScenes() {
+        if (defaultScenes == null) {
+            try {
+                FileReader fr = new FileReader(getDefaultScenesFile());
+                BufferedReader br = new BufferedReader(fr);
+
+                defaultScenes = JsonParser.parseReader(br).getAsJsonArray();
+
+                br.close();
+                fr.close();
+
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        return defaultScenes;
+    }
 }
